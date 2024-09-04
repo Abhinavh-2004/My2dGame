f56d1098f77a72bdd8325725a9f4126c6588188b
Abhinavh-2004 <abhinavhparthibanmit@gmail.com>
2024-09-05 00:51:08 +0530

    feat: Made rectangle for MC placeholder and movement mechanics

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -16,15 +16,23 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    int FPS = 60;
+
     Thread gameThread; //when you want to repeat drawing the screen again and again, threads can be extremely useful
 
+    KeyHandler keyH = new KeyHandler();
 
-
+    //set player default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
 
     }
     public void startGameThread(){
@@ -35,6 +43,71 @@ public class GamePanel extends JPanel implements Runnable{
     public void run() {
 
 
+        //we use the sleep method for the game loop
+        double drawInterval = 1000000000/FPS; //0.01666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval; //if the system time htis this time, raw the screeen again is the approach
+
         //the game loop exists here
+        while(gameThread != null){
+            //long currentTime = System.nanoTime(); //nano is a very small unit time, perfect for the purpose of a fast enough perception interval
+            //System.out.println(currentTime);
+            //System.out.println("The game loop is running");
+
+
+
+
+
+
+            //I update information such as character position
+            //Draw the screen with the updated information
+            update();
+            //the update and draw needs an interval to percieve and work
+            //find out the time now and also set the interval
+
+            repaint();
+
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000; //now this is milliseconds
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+                Thread.sleep((long)remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            /**As long as the game loop continues, it can keep updating and repainting*/
+
+
+        }
+    }
+    public void update(){
+        if(keyH.upPressed == true){
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed == true){
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g); //the parent class is Jpanel
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX,playerY,tileSize,tileSize); //the rectangle is just to check if it prints a thing on the window
+        g2.dispose(); //when drawing is done then dispose memory
+        /**To move the rectangel, we need keyboard input {dfound in KeyHandler class}*/
+
+
     }
 }
