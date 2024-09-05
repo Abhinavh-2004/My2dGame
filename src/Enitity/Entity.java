c1c5c82a100236d0674917b6da798150818fc104
Abhinavh-2004 <abhinavhparthibanmit@gmail.com>
2024-09-06 01:03:00 +0530

    !feat: The player control has been added along with entity

--- src/Enitity/Entity.java
+++ src/Enitity/Entity.java
@@ -1,9 +1,20 @@
 package Enitity;
 
+import java.awt.image.BufferedImage;
+
 /**This will be the parent class for all entities like players and monsters*/
 
 public class Entity {
 
     public int x,y;
     public int speed;
+
+    public BufferedImage up1,up2,down1,down2,left1,left2,right1,right2;
+    public String direction;
+
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+
+
+
 }
